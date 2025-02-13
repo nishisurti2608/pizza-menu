ac304ce2eb348e79016fd85c29ad790dd0f39690
Nishi Surti <01nishisurti@gmail.com>
2025-02-13 11:48:03 -0500

    Done

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,7 +1,21 @@
 const Footer = () => {
+  const hour = new Date().getHours();
+  const openHour = 10;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
   return (
     <footer className="footer">
-      {new Date().toLocaleDateString()} We are open
+      {isOpen ? (
+        <div className="order">
+          <p>
+            We are currently Open until {closeHour % 12 || 12} PM! Come visit us
+            Order Online!
+          </p>
+          <button className="btn">Order Now</button>
+        </div>
+      ) : (
+        <p>Sorry We are Closed !</p>
+      )}
     </footer>
   );
 };
--- src/Components/Menu.jsx
+++ src/Components/Menu.jsx
@@ -5,11 +5,24 @@ const Menu = () => {
   return (
     <main className="menu">
       <h2>Our Menu</h2>
-      <div className="pizzas">
-        {pizzaData.map((pizza) => (
-          <Pizza key={pizza.name} data={pizza} />
-        ))}
-      </div>
+
+      {pizzaData.length > 0 ? (
+        <>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Porro,
+            placeat! Illum sapiente id perferendis dignissimos blanditiis a
+            necessitatibus, nostrum voluptas!
+          </p>
+
+          <ul className="pizzas">
+            {pizzaData.map((pizza) => (
+              <Pizza key={pizza.name} data={pizza} />
+            ))}
+          </ul>
+        </>
+      ) : (
+        <p>We are preparing fresh Pizzas for you!</p>
+      )}
     </main>
   );
 };
